4eba5e40352e9b8b6e4fd08fcd570f5f2001e5c2
gaurav_si <reachgauravarora@outlook.com>
2015-10-17 08:43:59 -0700

    update made proper

--- app/controllers/rawMaterialController.php
+++ app/controllers/rawMaterialController.php
@@ -118,7 +118,10 @@ class rawMaterialController extends BaseController {
 	{
 		$date=date("Y-m-d");
 		$data= Input::all();
-		$data_array = array(
+
+
+
+		$data_array_update = array(
 			'receipt_code' => $data['receiptCode'],
 			'date' => $date,
 			'size' => $data['size'],
@@ -135,14 +138,41 @@ class rawMaterialController extends BaseController {
 		);
 		$update_response= DB::table('raw_material')
 							->where('internal_no',$data['internal_no'])
-							->update($data_array);
+							->update($data_array_update);
 
-		$get_record= RawMaterial::getRecord($data['internal_no']);
-		dd("updated");
+//
+////		array(1) { [0]=> object(stdClass)#242 (14) { ["internal_no"]=> int(5) ["receipt_code"]=> string(5) "12345"
+////		# ["date"]=> string(10) "2015-10-17" ["size"]=> int(100) ["manufacturer"]=> string(12) "gaurav arora"
+////		# ["heat_no"]=> string(4) "4436" ["weight"]=> int(6653) ["left_over_weight"]=> string(0) ""
+////		# ["pur_order_no"]=> string(4) "4656" ["pur_order_date"]=> string(10) "0000-00-00" ["invoice_no"]=> string(5) "46565"
+////		# ["invoice_date"]=> string(10) "0000-00-00" ["material_grade"]=> string(7)
+////		# "Grade 1" ["raw_material_type"]=> string(6) "Type 1" } }
+//
+//
+//
+//
+//		$get_record_array= RawMaterial::getRecord($data['internal_no']);
+//		dd($get_record_array['internal_no']);
+//
+//			$to_be_sent= new stdClass();
+//			$to_be_sent->receipt_code= $get_record->receiptCode;
+//			$to_be_sent->size = $get_record->size;
+//			$to_be_sent->manufacturer= $get_record->Manufacturer;
+//			$to_be_sent->heat_no=$get_record->heatNo;
+//			$to_be_sent->weight=$get_record->weight;
+//			$to_be_sent->pur_order_no=$get_record->purchaseNo;
+//			$to_be_sent->pur_order_date=$get_record->purchaseDate;
+//			$to_be_sent->invoice_no=$get_record->invoiceNo;
+//			$to_be_sent->left_over_material=$get_record->left_over_weight;
+//			$to_be_sent->invoice_date=$get_record->invoiceDate;
+//			$to_be_sent->material_grade=$get_record->materialGrade;
+//			$to_be_sent->raw_material_type= $get_record->materialType;
+//
+//
+//		//problem in fetching results
 
-		//problem in fetching results
-
-		return View::make('rawMaterial.confirm')->with('confirmation',$get_record);
+		$get_record_array= RawMaterial::getRecord($data['internal_no']);
+		return View::make('rawMaterial.confirm_update')->with('confirmations',$get_record_array);
 
 
 	}
